178807129b42974713703669cdb5571121f29ace
roh eun chan <planic324@douzone.com>
2022-06-09 01:40:51 +0900

    [add] sass set

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+'use strict';
+var gulp = require('gulp');
+var gulpsass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var cleancss = require('gulp-clean-css');
+var csslint = require('gulp-csslint');
+var htmlReporter = require('gulp-csslint-report');
+var replace = require('gulp-replace');
+var autoprefixer = require('gulp-autoprefixer');
+var through2 = require('through2');
+
+// ★★★★ autoprefixer 옵션: 브라우저 버전 지정
+const apfBrwsowsers = [
+  'ie >= 8', // 익스플로러 버전 8 이상
+];
+
+gulp.task('sass', function (done) {
+    gulp.src('scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(gulpsass({sourcemap: true, outputStyle: 'expanded'}).on('error', gulpsass.logError))
+        // .pipe(prefix({browser:["last 2 version", "> 1%", "ie 8", "ie 7"],cascade:false,flexbox:false}))
+        // .pipe(cleancss({format: 'keep-breaks' }))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('css/'));
+
+    done();
+});
+gulp.task('sass:watch', function (done) {
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+
+    done();
+});
+
+gulp.task('planic', function (done) {
+    gulp.src('scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(gulpsass({sourcemap: true, outputStyle: 'expanded'}).on('error', gulpsass.logError))
+        // .pipe(prefix({browser:["last 2 version", "> 1%", "ie 8", "ie 7"],cascade:false,flexbox:false}))
+        .pipe(cleancss({format: 'keep-breaks' }))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('css/'));
+
+    done();
+});
+gulp.task('planic:watch', function (done) {
+    gulp.watch('scss/**/*.scss', gulp.series('planic'));
+    done();
+});
+
+
+// gulp.task(
+//     "default",
+//     gulp.parallel("gulpsass") 
+// );
