454a1d31c78c1654f022d32db0678db0c2de7ca2
노은찬 <planic324@gmail.com>
2022-07-21 18:43:56 +0900

    js 파일 생성

--- js/planic.js
+++ js/planic.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
-	
+	// fullpage.js
+	new fullpage('#fullpage', {
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+		fadingEffect: true,
+		anchors:['main', 'introduce', 'spec', 'history', 'skills'],
+	});
+
+	// history box select.js
 	$(".historyWrap .historyBox").hide();
 	$(".historyWrap .incomms").show();
 	
@@ -9,7 +18,7 @@ $(document).ready(function(){
 
 	var i = $(this).index();
 	// 탭 영역 인터랙션
-	$(".tab_box .btn_tab").removeClass("select");
+	$(".historyTab .btn_tab").removeClass("select");
 	$(this).toggleClass("select");
 	// 클릭 후 리스트 삭제
 	$(".historyWrap .historyBox").hide();
@@ -27,4 +36,51 @@ $(document).ready(function(){
 		}
 	});
 
+	// slick.js
+	$('.historyList').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		// accessibility: true,
+		dots: true,
+		// autoplay: true,
+		// autoplaySpeed: 5000,
+		// speed: 500,
+		infinite: false,
+		// cssEase: 'linear',
+		arrows: true,
+		variableWidth: true
+	});
+
+	// slick 너비
+	$(document).ready(function() {
+		var slick_boxWidth = $(".slick-list").outerWidth();
+		$('.slick-slide').width(slick_boxWidth + 'px');
+	});
+
+	// header 색상 변경
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+		//console.log(scroll);
+		if (scroll >= 80) {
+			//console.log('a');
+			$(".header").addClass("v2");
+		} else {
+			//console.log('a');
+			$(".header").removeClass("v2");
+		}
+	});
+
+	// 모바일 메뉴
+	$(window).ready(function(){
+		$(".global__menu .btn__menu--mobile").click(function(){
+			$(this).parents(".global__menu").toggleClass("open")
+		});
+	
+		$(".global__menuList--item a").click(function(){
+			$(this).parents(".global__menu").removeClass("open")
+		});
+	})
+
+
+	// end
 })
