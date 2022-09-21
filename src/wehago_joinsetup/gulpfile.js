60f09826d7b6201408fb07cadb70ca327b0b738d
RohEunChan <nong0324@naver.com>
2022-09-21 10:28:31 +0900

    [추가] wehago - 초기설정

---
+++ src/wehago_joinsetup/gulpfile.js
@@ -0,0 +1,33 @@
+"use strict";
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var cleancss = require("gulp-clean-css");
+var autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("join_setup", function (done) {
+  gulp
+    .src("scss/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({ sourcemap: true, outputStyle: "expanded" }).on(
+        "error",
+        sass.logError
+      )
+    )
+    // .pipe(prefix({browser:["last 2 version", "> 1%", "ie 8", "ie 7"],cascade:false,flexbox:false}))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(cleancss({ format: "keep-breaks" }))
+    .pipe(sourcemaps.write("./maps"))
+    .pipe(gulp.dest("../../../css/svc"));
+  done();
+});
+
+gulp.task("join_setup:watch", function (done) {
+  gulp.watch("scss/**/*.scss", gulp.series("join_setup"));
+  done();
+});
